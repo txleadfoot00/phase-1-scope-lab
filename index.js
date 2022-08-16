b79aa13dd5cce40992dfc460937f823e4008ba73
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-15 20:44:01 -0700

    commit tree fc62f5d14c13ff128b4bedfd2254b3a78dc16669

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName() {
+    customerName1 = (customerName.toUpperCase());
+    customerName = customerName1;
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "dave";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "jack";
+}
